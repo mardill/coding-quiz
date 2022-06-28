603fc113ebb14b2ba23589598aa5673b33e072ee
Mary Dillon <mary.dillon09@gmail.com>
2022-06-27 19:22:54 -0700

    updates hidden start page agter button click, question js logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,31 @@
-var startPage = document.querySelector("#startPage");
+
+var start = document.querySelector("#startPage");
 var startButton = document.querySelector("#startButton");
+var card = document.querySelector("#question-card")
 var questionEl = document.querySelector("#question");
 var questionOpEl = document.querySelector("#question-options");
 var opAns1 = document.querySelector("#ans1");
 var opAns2 = document.querySelector("#ans2");
 var opAns3 = document.querySelector("#ans3");
 var opAns4 = document.querySelector("#ans4");
-var ans = document.querySelector("#result");
+var correct = document.querySelector("#winlose");
 
-startButton.addEventListener("click", function(event){
-    
 
+
+function startGame(){
+    startButton.style.display = 'none'
+    start.style.display = 'none'
+    questionEl.style.display = 'block'
+    opAns1.style.display = 'block'
+    opAns2.style.display = 'block'
+    opAns3.style.display = 'block'
+    opAns4.style.display = 'block'
 }
-)
+
+
+startButton.addEventListener("click", startGame)
+
+
 
 var questions = [
     {
@@ -34,41 +47,37 @@ opAns3.innerText = questions[0].a[2].text;
 opAns4.innerText = questions[0].a[3].text;
 
 
-opAns1.addEventListener("click", checkAnswer1)
+opAns1.addEventListener("click", checkAnswer)
 
-function checkAnswer1(){
+function checkAnswer(){
     if(questions[0].a[0].correct === true){
-        return ans.innerText = "Correct!";
+        return correct.innerText = "Correct!";
     }
-    else return ans.innerText = "Wrong!";
+    else {return correct.innerText = "Wrong!";
     }
 
-opAns2.addEventListener("click", checkAnswer2)
-
-function checkAnswer2(){
     if(questions[0].a[1].correct === true){
-        return ans.innerText = "Correct!";
+        return correct.innerText = "Correct!";
     }
-    else return ans.innerText = "Wrong!";
+    else {return correct.innerText = "Wrong!";
     }
 
-opAns3.addEventListener("click", checkAnswer3)
-
-function checkAnswer3(){
     if(questions[0].a[2].correct === true){
-        return ans.innerText = "Correct!";
+        return correct.innerText = "Correct!";
     }
-    else return ans.innerText = "Wrong!";
+    else {return correct.innerText = "Wrong!";
     }
 
-opAns4.addEventListener("click", checkAnswer4)
-
-function checkAnswer4(){
     if(questions[0].a[3].correct === true){
-        return ans.innerText = "Correct!";
+        return correct.innerText = "Correct!";
     }
-    else return ans.innerText = "Wrong!";
+    else {return correct.innerText = "Wrong!";
     }
+
+}
+
+
+
 
 
 
